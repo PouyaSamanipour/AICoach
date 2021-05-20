04adf98fcc24f9477f11e813486974b1e1ac9191
Pouya Samanipour <78120159+PouyaSamani@users.noreply.github.com>
2021-05-20 18:33:54 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -6,9 +6,11 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 outputPin=12
 Start=22
-pushbutton=40
+pushbutton=13
+Recording=36
 GPIO.setup(outputPin,GPIO.OUT)
 GPIO.setup(Start,GPIO.OUT)
+GPIO.setup(Recording,GPIO.OUT)
 GPIO.setup(pushbutton,GPIO.IN)
 GPIO.output(outputPin,0)
 GPIO.output(Start,0)
@@ -17,6 +19,7 @@ import json
 import trt_pose.coco
 with open('human_pose.json', 'r') as f:
     human_pose = json.load(f)
+
 topology = trt_pose.coco.coco_category_to_topology(human_pose)
 num_parts = len(human_pose['keypoints'])
 num_links = len(human_pose['skeleton'])
@@ -26,14 +29,14 @@ model.load_state_dict(torch.load(MODEL_WEIGHTS))
 WIDTH = 224
 HEIGHT = 224
 data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
-#model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
+model_trt = torch2trt.torch2trt(model, [data], fp16_mode=True, max_workspace_size=1<<25)
 OPTIMIZED_MODEL = 'resnet18_baseline_att_224x224_A_epoch_249_trt.pth'
-#torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
+torch.save(model_trt.state_dict(), OPTIMIZED_MODEL)
 from torch2trt import TRTModule
 model_trt = TRTModule()
 model_trt.load_state_dict(torch.load(OPTIMIZED_MODEL))
 #######
-#t0 = time.time()
+#t0 = time.time()o
 #torch.cuda.current_stream().synchronize()
 #for i in range(50):
 #y = model_trtmo(data)
@@ -71,6 +74,7 @@ from jetcam.utils import bgr8_to_jpeg
 #camera = USBCamera(width=WIDTH, height=HEIGHT, capture_fps=30)
 camera = CSICamera(width=WIDTH, height=HEIGHT, capture_fps=30)
 camera.running = True
+
 import ipywidgets
 from IPython.display import display
 
@@ -86,10 +90,9 @@ def execute(change):
     counts, objects, peaks = parse_objects(cmap, paf)#, cmap_threshold=0.15, link_threshold=0.15)
     draw_objects(image, counts, objects, peaks)
     image_w.value = bgr8_to_jpeg(image[:, ::-1, :])
-    return peaks
+    return peaks, image
 button=0
-
-#Classifier
+#creating csv file
 import os.path
 from os import path
 import pandas as pd
@@ -100,91 +103,145 @@ while path.exists(df_name):
   file_num = int(df_name[14:-4])
   new_file_num = file_num + 1
   df_name = df_name_start+str(new_file_num)+df_name_end
+#Classifier
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 GPIO.setmode(GPIO.BOARD)
-GPIO.output(outputPin,1)
-time.sleep(5)
-GPIO.output(outputPin,0)
+time.sleep(1)
+#GPIO.output(outputPin,0)
 GPIO.output(Start,1)
-time.sleep(5)
+time.sleep(3)
 import numpy as np
 counter=0
 k=0
 TH=0.17
-Data=np.zeros((10000,37))
+Data=np.zeros((1000000,37))
 status=[]
-display(image_w)
+timestamp=[]
+#display(image_w)
 i=0
+state_old="Not detected"
 while (1==1):
+    time.sleep(1)
     x=GPIO.input(pushbutton)
     if(x==0):
-        GPIO.output(Start,0)
+        #GPIO.output(Start,0)
+        GPIO.output(Recording,1)
         time.sleep(0.5)
-        GPIO.output(Start,1)
+        GPIO.output(Recording,0)
         peak=execute({'new': camera.value})
         for i in range (18):
-            Data[counter][k:k+2]=peak[0][i][0]
+            Data[counter][k:k+2]=peak[0][0][i][0]
             k=k+2
 
         if (Data[counter][8]==0 and Data[counter][12]==0) or (Data[counter][6]==0 and Data[counter][10]==0):
             print("Not detected")
             status.append("Not detected")
+            state_new="Not detected"
         elif (Data[counter][24]==0 and Data[counter][22]==0):
-            print("Seated1")
-            status.append("Seated1")
+            if (Data[counter][10]<=0.15 or Data[counter][12]<=0.15):
+                status.append("Standing position new")
+                print("Standing position new")
+                state_new="Standing position"
+            else:
+                status.append("Seated1")
+                print("Seated")
+                state_new="Seated"
         elif (Data[counter][24]!=0 and Data[counter][22]==0):
             if Data[counter][28]!=0:
                 if (Data[counter][28]-Data[counter][24])>=TH:
                     print("Standing position1")
                     status.append("Standing position1")
+                    state_new="Standing position"
                 else:
                     if ((Data[counter][28]-Data[counter][24])>0.65*(Data[counter][24]-Data[counter][12])):
                         print("Standing position2")
                         status.append("Standing position2")
+                        state_new="Standing position"
                     else:
                         print("seated2")
                         status.append("seated2")
-                    
+                        state_new="Seated"
             else:
+                if (Data[counter][10]<=0.20 or Data[counter][12]<=0.2):
+                    status.append("Standing position new_1")
+                    print("Standing position new_1")
+                    state_new="Standing position"
+                else:
                     print("seated3")
                     status.append("seated3")
+                    state_new="Seated"
         elif (Data[counter][24]==0 and Data[counter][22]!=0):
             if Data[counter][26]!=0:
                 if (Data[counter][26]-Data[counter][22])>=TH:
                     print("Standing position3")
                     status.append("Standing position3")
+                    state_new="Standing position"
                 else:
                     if((Data[counter][26]-Data[counter][22])>0.65*(Data[counter][22]-Data[counter][10])):
                         print("Standing position4")
                         status.append("Standing position4")
+                        state_new="Standing position"
                     else:
                         print("seated4")
                         status.append("seated4")
+                        state_new="Seated"
    
             else:
-                print("seated5") 
-                status.append("seated5")
+                    if (Data[counter][10]<=0.20 or Data[counter][12]<=0.2):
+                        status.append("Standing position new_2")
+                        print("Standing position new_2")
+                        state_new="Standing position"
+                    else:
+                        print("seated5") 
+                        status.append("seated5")
+                        state_new="Seated"
         elif (Data[counter][24]!=0 and Data[counter][22]!=0):
             if Data[counter][28]!=0:
                 if (Data[counter][28]-Data[counter][24]>=TH) or (Data[counter][26]-Data[counter][22]>=TH):
                     print("Standing position5")
                     status.append("Standing position5")
+                    state_new="Standing position"
                 else:
                     if ((Data[counter][26]-Data[counter][22])>0.65*(Data[counter][22]-Data[counter][10])) or ((Data[counter][28]-Data[counter][24])>0.65*(Data[counter][24]-Data[counter][12])):
                         print("Standing position6")
                         status.append("Standing position6")
+                        state_new="Standing position"
                     else:    
                         print("seated6")
                         status.append("seated6")
+                        state_new="Seated"
             else:
-                print("seated7")
-                status.append("seated7")
+                if (Data[counter][10]<=0.20 or Data[counter][12]<=0.2):
+                    status.append("Standing position new_3")
+                    print("Standing position new_3")
+                    state_new="Standing position"
+                else:
+                    print("seated7")
+                    status.append("seated7")
+                    state_new="Seated"
         counter=counter+1
+        current_time=datetime.now()
+        timestamp.append(current_time)
+        d={'col1':status,'col2':timestamp}
+        df = pd.DataFrame(data=d)
+        df.to_csv(df_name)
         k=0
-        time.sleep(3)
+        time.sleep(2)
+        # saving images
+        if (counter%900==1 or state_new!=state_old):
+            Image_name=current_time.strftime("%m_%d_%Y,%H:%M:%S")
+            #Image_sufix=".png"
+            #Image_name+=Image_sufix
+            from PIL import Image as im
+            data=im.fromarray(peak[1])
+            data.save('Image_'+Image_name+'.png')
+        state_old=state_new
         camera.unobserve_all()
     elif(x==1):
-        GPIO.output(Start,0)
+        GPIO.output(Start,1)
+        GPIO.output(Recording,0)
         camera.unobserve_all()
 i=i+1        
+
